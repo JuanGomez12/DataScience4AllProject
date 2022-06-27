44ee923fd048b45b09c23f2e128fa25a307e73e5
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2022-06-27 00:33:42 -0400

    Add ml_pipeline and example function

--- core/views.py
+++ core/views.py
@@ -1,12 +1,22 @@
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-import requests
-from django.http import Http404
-from django.shortcuts import render, redirect
-from django.http import HttpResponseBadRequest
+import sys
+from typing import Optional
 
+import numpy as np
+import requests
+from django.http import Http404, HttpResponseBadRequest
+from django.shortcuts import redirect, render
+from joblib import load
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Add the scripts directory to sys.path to be able to load the pipeline correctly
+sys.path.append('scripts')
+from utils.preprocessing_utils import preprocess_json
+
+ml_pipeline = load('scripts/data/output/prediction_pipeline.pickle')
+    
 
 class Post_APIView(APIView):
 
@@ -20,7 +30,9 @@ class Post_APIView(APIView):
     def post(self, request, format=None):
         post_data = request.GET
         data = {'Edad':post_data['Edad'],'Genero':post_data['Genero'],'GrupoEtnico':post_data['GrupoEtnico'],'AreaResidencial':post_data['AreaResidencial'],'EstadoCivil':post_data['EstadoCivil'],'TSangre':post_data['TSangre'],'Tipo':post_data['Tipo'],'Plan':post_data['Plan'],'examenesRealizados':post_data['examenesRealizados'],'Valores':post_data['Valores'],'fecha':post_data['fecha']}
-        prediction = {'respuesta':'respuesta satisfactoria'}
+        
+        prediction = ml_pipeline.predict(preprocess_json(data))
+        prediction = {'respuesta':prediction[0]}
         if data:
             return Response(prediction, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -36,3 +48,27 @@ def prueba(request):
             data =  response
 
     return render(request, "core/prueba.html", {'data':data})
+
+def test_ml_model(sample_path:Optional[str] = None) -> np.array:
+    """Tests the functionality of the ML model.
+    The model expects a dictionary with keys and formatting equal to the one
+    found in the sample_example json file, located in scripts/utils/.
+
+    Args:
+        sample_path (Optional[str], optional): Path to sample file. If None is
+            given, it uses the example JSON file located in scripts/utils/.
+            Defaults to None.
+        
+
+    Returns:
+        np.array: Numpy array containing the prediction for the supplied example.
+    """
+    import json
+    if sample_path is None:
+        sample_path = 'scripts/utils/sample_example.json'
+    with open(sample_path) as in_file:
+        sample_data = json.load(in_file)
+
+    prediction = ml_pipeline.predict(
+        X=preprocess_json(sample_data))
+    return prediction
