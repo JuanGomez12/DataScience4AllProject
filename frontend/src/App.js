02c46aae2050d51e8bcd742f44006cd0f5146ab5
David Santiago Garcia Chicangana <dsgarcia@unicauca.edu.co>
2022-07-02 13:36:03 -0500

    argon dashboard templated included

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,53 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navegationbar from './components/Navegationbar/Navegationbar';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Home from './pages/Home';
 import Prediction from './pages/Prediction';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
+import "./assets/plugins/nucleo/css/nucleo.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/css/argon-dashboard-react.css";
+
+import AdminLayout from './layouts/AdminLayout';
+import Homepage from './layouts/Homepage';
+import Index from './pages/Index';
 
 function App() {
+
   return (
-    <div className="App">
+    // <div className="App">
       <Router>
-        <Navegationbar></Navegationbar>
+        {/* <Navegationbar></Navegationbar> */}
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/predict' element={<Prediction/>}/>
+          <Route path="/admin/*" element={<AdminLayout /> } />
+          <Route path="/home" element={<Homepage /> } />
+          <Route path='/*' element={<Homepage/>}/>
+          {/* <Route path="/*" element={<Homepage /> } /> */}
+          {/* <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/predict' element={<Prediction/>}/> */}
         </Routes>
       </Router>
-    </div>
+    // </div>
   );
 }
 
---
+++ frontend/src/components/Navbars/HomepageNavbar.js
@@ -0,0 +1,82 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import { Link } from "react-router-dom";
+// reactstrap components
+import {
+  UncontrolledCollapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+const HomepageNavbar = () => {
+  return (
+    <Navbar className="navbar-top navbar-horizontal navbar-dark" expand="md">
+      <Container className="px-4">
+        <NavbarBrand to="/home" tag={Link}>
+          <img
+            alt="..."
+            src="/icons/dd_white.png"
+          />
+        </NavbarBrand>
+        <button className="navbar-toggler" id="navbar-collapse-main">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        {/* Menu para dispositivos con menor tama;o de pantalla */}
+        <UncontrolledCollapse navbar toggler="#navbar-collapse-main">
+          <div className="navbar-collapse-header d-md-none">
+            <Row>
+              <Col className="collapse-brand" xs="6">
+                <Link to="/home">
+                  <img
+                    alt="..."
+                    src="icons/logo_2.png"
+                  />
+                </Link>
+              </Col>
+              <Col className="collapse-close" xs="6">
+                <button className="navbar-toggler" id="navbar-collapse-main">
+                  <span />
+                  <span />
+                </button>
+              </Col>
+            </Row>
+          </div>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink className="nav-link-icon" to="/admin" tag={Link}>
+                <i className="ni ni-planet" />
+                <span className="nav-link-inner--text">Dashboard</span>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </UncontrolledCollapse>
+        {/* Menu para dispositivos con menor tama;o de pantalla */}
+        
+      </Container>
+    </Navbar>
+  );
+};
+
+export default HomepageNavbar;
